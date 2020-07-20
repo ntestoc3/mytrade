9106a5d3acc1b2fc44fa82a2d70a3fcc51332d45
ntestoc3 <ntestoc3@outlook.com>
2020-07-20 21:16:16 +0800

    add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((eval . (setq cider-preferred-build-tool 'shadow-cljs))
+         (eval . (setq cider-default-cljs-repl  'shadow))
+         (eval . (setq cider-shadow-default-options "browser")))))
